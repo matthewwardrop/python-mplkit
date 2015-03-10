a018dd396d1059f71ddd5b08bbb4c9b9f4d1ba31
Matthew Wardrop <mister.wardrop@gmail.com>
2015-03-11 10:38:25 +1100

    Remove whitespace.

--- mplstyles/plots.py
+++ mplstyles/plots.py
@@ -5,20 +5,20 @@ import numpy as np
 
 def contour_image(x,y,Z,cmap=None,vmax=None,vmin=None,interpolation='nearest',contour_opts={},label_opts={},imshow_opts={},clegendlabels=[],label=False):
 	ax = plt.gca()
-	
+
 	x_delta = float((x[-1]-x[0]))/(len(x)-1)/2.
 	y_delta = float((y[-1]-y[0]))/(len(y)-1)/2.
-	
+
 	extent=(x[0],x[-1],y[0],y[-1])
-	
+
 	extent_delta = (x[0]-x_delta,x[-1]+x_delta,y[0]-y_delta,y[-1]+y_delta)
-	
+
 	ax.set_xlim(x[0],x[-1])
 	ax.set_ylim(y[0],y[-1])
-	
+
 	if cmap is None:
 		cmap = colormap.reverse(cm.Blues)
-	
+
 	Z = Z.transpose()
 
 	#plt.contourf(X,Y,self.pdata,interpolation=interpolation)
@@ -37,5 +37,5 @@ def contour_image(x,y,Z,cmap=None,vmax=None,vmin=None,interpolation='nearest',co
 		for i in range(len(clegendlabels)):
 			CS.collections[i].set_label(clegendlabels[i])
 		#ax.legend()
-	
+
 	return cs, CS
